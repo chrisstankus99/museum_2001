d96da81a4777a47c1ae0c7d33f6a1e1432c04b3f
Chris Stankus <chrisstankus99@gmail.com>
2020-02-19 09:15:12 -0700

    Add test for interests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -18,14 +18,12 @@ class PatronTest < Minitest::Test
     assert_equal "Bob", @patron_1.name
     assert_equal 20, @patron_1.spending_money
   end
+
+  def test_patron_has_no_interests
+    assert_equal [], @patron_1.interests
+  end
 end
 
-# pry(main)> patron_1.name
-# # => "Bob"
-#
-# pry(main)> patron_1.spending_money
-# # => 20
-#
 # pry(main)> patron_1.interests
 # # => []
 #
