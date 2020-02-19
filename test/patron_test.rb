e1edcceb466e090ab3f34dac4d04e1a9f7ceb729
Chris Stankus <chrisstankus99@gmail.com>
2020-02-19 09:10:49 -0700

    Add first test and setup for patron

---
+++ test/patron_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+require './lib/exhibit'
+
+
+class PatronTest < Minitest::Test
+
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @patron_1
+  end
+
+  def test_attributes
+  end
+end
+
+# pry(main)> patron_1.name
+# # => "Bob"
+#
+# pry(main)> patron_1.spending_money
+# # => 20
+#
+# pry(main)> patron_1.interests
+# # => []
+#
+# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
+#
+# pry(main)> patron_1.add_interest("Gems and Minerals")
+#
+# pry(main)> patron_1.interests
+# # => ["Dead Sea Scrolls", "Gems and Minerals"]
+# ```
